be39834b425ca9e3f4514f83aa81c97fb57ed803
DeadShotSLS <klsameerapathmakumara@gmail.com>
2020-05-17 20:51:00 +0530

    Update your_orders.php

--- your_orders.php
+++ your_orders.php
@@ -154,6 +154,51 @@ only screen and (max-width: 760px),
 	</head>
 
 <body>
+    <div class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container-fluid">	
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapse" aria-expanded="false">
+                        <span class="sr-only"> navigation toggle</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a href="#" class="navbar-brand">Wrapit bags</a>
+                </div>
+            <div class="collapse navbar-collapse" id="collapse">
+                <ul class="nav navbar-nav">
+                    <li><a href="index.php"><span class="glyphicon glyphicon-home"></span>Home</a></li>
+                    <li><a href="index.php"><span class="glyphicon glyphicon-modal-window"></span>Product</a></li>
+                    <li style="width:300px;left:10px;top:10px;"><input type="text" class="form-control" id="search"></li>
+                    <li style="top:10px;left:20px;"><button class="btn btn-primary" id="search_btn">Search</button></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-shopping-cart"></span>Cart<span class="badge">0</span></a>
+                        <div class="dropdown-menu" style="width:400px;">
+                            <div class="panel panel-success">
+                                <div class="panel-heading">
+                                    <div class="row">
+                                        <div class="col-md-3">Sl.No</div>
+                                        <div class="col-md-3">Product Image</div>
+                                        <div class="col-md-3">Product Name</div>
+                                        <div class="col-md-3">Price in Rs.</div>
+                                    </div>
+                                </div>
+                                <div class="panel-body">
+                                    <div id="cart_product">
+                                    
+                                    </div>
+                                </div>
+                                <!-- <div class="panel-footer"></div> -->
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <p><br/></p>
+	<p><br/></p>
     <div class="site-wrapper animsition" data-animsition-in="fade-in" data-animsition-out="fade-out">
         <!--header starts-->
         <div class="page-wrapper" style="padding: 0">
